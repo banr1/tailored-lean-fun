a38a9f07cca6e40fd8174589c99f6212dbfcc501
Vitaly Aksenov <aksenov@logicalintelligence.com>
2026-02-23 19:30:07 +0300

    Theorem 6 has formalized statement.

---
+++ LeanFun/theorem6.lean
@@ -0,0 +1,13 @@
+import LeanFun.Definitions
+
+open abelian
+
+def theorem6
+  (M : Finset (FreeAbelianMonoid 1)) :
+  ∃ C₁ C₂ : ℝ,
+    0 < C₁ ∧ 0 < C₂ ∧
+    (∀ (s : ℕ), (s ≥ 1) →
+      (Ball (Int.toNat <| Int.ceil <| C₁ * s) (A 1) ⊆ Ball s (M ∪ A 1)) ∧
+      ¬ (Ball (1 + Int.toNat <| Int.floor <| C₂ * s) (A 1) ⊆ Ball s (M ∪ A 1))
+    ) :=
+  sorry
