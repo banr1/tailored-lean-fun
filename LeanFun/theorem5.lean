d9274ae598248cc22302d6679c7b0e22ac6a73fe
Vitaly Aksenov <aksenov@logicalintelligence.com>
2026-02-23 19:48:39 +0300

    Theorem 5 is formalized.

---
+++ LeanFun/theorem5.lean
@@ -0,0 +1,22 @@
+import LeanFun.Definitions
+
+open abelian
+
+def DoublyMacroSet (b : ℕ) : Set (FreeAbelianMonoid 1) :=
+  { m | ∃ i : Fin 1, ∃ j : ℕ, 0 ≤ j ∧ m = Multiset.replicate (b ^ (b ^ j)) i }
+
+theorem theorem5
+  (b : ℕ)
+  (hb : 2 ≤ b) :
+  let M := DoublyMacroSet b
+  (∃ (d1 d2 : ℝ), ∀ (x : ℕ), (x ≥ b ^ b) → 0 < d1 ∧ 0 < d2
+      ∧ d1 * (Real.log (Real.log x)) ≤ (M ∩ (Ball x (A 1))).ncard
+      ∧ (M ∩ (Ball x (A 1))).ncard ≤ d2 * (Real.log (Real.log x))) ∧
+  (∃ C₁ C₂ : ℝ,
+    0 < C₁ ∧ 0 < C₂ ∧
+    (∀ (s : ℕ), (s ≥ 1) →
+      let rs := Real.rpow s ((b : ℝ) / (b - 1))
+      (Ball (Int.toNat <| Int.ceil <| C₁ * rs) (A 1) ⊆ Ball s (M ∪ (A 1))) ∧
+        ¬ (Ball (Int.toNat <| Int.floor <| C₂ * rs)) (A 1) ⊆ Ball s (M ∪ (A 1)))
+    ) :=
+  sorry
