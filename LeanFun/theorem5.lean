752478df578540a856f746d78ff1b6edbb3b05b0
Vitaly Aksenov <aksenov@logicalintelligence.com>
2026-02-25 17:34:34 +0300

    Added lower bound for s in theorem 5.

--- LeanFun/theorem5.lean
+++ LeanFun/theorem5.lean
@@ -3,7 +3,10 @@ import LeanFun.Definitions
 open abelian
 
 def DoublyMacroSet (b : ℕ) : Set (FreeAbelianMonoid 1) :=
-  { m | ∃ i : Fin 1, ∃ j : ℕ, 0 ≤ j ∧ m = Multiset.replicate (b ^ (b ^ j)) i }
+  { m | ∃ i : Fin 1, ∃ j : ℕ, m = Multiset.replicate (b ^ (b ^ j)) i }
+
+lemma simple_lemma {A B : Prop} : A ∧ B → B :=
+  fun h : A ∧ B => And.right h
 
 theorem theorem5
   (b : ℕ)
@@ -12,9 +15,9 @@ theorem theorem5
   (∃ (d1 d2 : ℝ), ∀ (x : ℕ), (x ≥ b ^ b) → 0 < d1 ∧ 0 < d2
       ∧ d1 * (Real.log (Real.log x)) ≤ (M ∩ (Ball x (A 1))).ncard
       ∧ (M ∩ (Ball x (A 1))).ncard ≤ d2 * (Real.log (Real.log x))) ∧
-  (∃ C₁ C₂ : ℝ,
+  (∃ C₁ C₂ B : ℝ,
     0 < C₁ ∧ 0 < C₂ ∧
-    (∀ (s : ℕ), (s ≥ 1) →
+    (∀ (s : ℕ), (s ≥ B) →
       let rs := Real.rpow s ((b : ℝ) / (b - 1))
       (Ball (Int.toNat <| Int.ceil <| C₁ * rs) (A 1) ⊆ Ball s (M ∪ (A 1))) ∧
         ¬ (Ball (1 + Int.toNat <| Int.floor <| C₂ * rs)) (A 1) ⊆ Ball s (M ∪ (A 1)))
