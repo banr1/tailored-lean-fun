b23fe5bd5d6463af6d06e26025de90e93b26e3d3
Vitaly Aksenov <aksenov@logicalintelligence.com>
2026-02-26 16:20:18 +0300

    Theorem is split.

--- LeanFun/theorem5.lean
+++ LeanFun/theorem5.lean
@@ -5,8 +5,30 @@ open abelian
 def DoublyMacroSet (b : ℕ) : Set (FreeAbelianMonoid 1) :=
   { m | ∃ i : Fin 1, ∃ j : ℕ, m = Multiset.replicate (b ^ (b ^ j)) i }
 
-lemma simple_lemma {A B : Prop} : A ∧ B → B :=
-  fun h : A ∧ B => And.right h
+theorem theorem5_density (b : ℕ) (hb : 2 ≤ b) :
+  ∃ (d1 d2 : ℝ), ∀ (x : ℕ), x ≥ b ^ b →
+    0 < d1 ∧ 0 < d2 ∧
+      d1 * (Real.log (Real.log x)) ≤ ((DoublyMacroSet b) ∩ Ball x (A 1)).ncard ∧
+      ((DoublyMacroSet b) ∩ Ball x (A 1)).ncard ≤ d2 * (Real.log (Real.log x)) := by
+      sorry
+
+theorem theorem5_lower_bound (b : ℕ) (hb : 2 ≤ b) :
+  (∃ C₁ B : ℝ,
+    0 < C₁ ∧
+    (∀ (s : ℕ), (s ≥ B) →
+      let rs := Real.rpow s ((b : ℝ) / (b - 1))
+      (Ball (Int.toNat <| Int.ceil <| C₁ * rs) (A 1) ⊆ Ball s (M ∪ (A 1))))
+    ) := by
+    sorry
+
+theorem theorem5_upper_bound (b : ℕ) (hb : 2 ≤ b) :
+  (∃ C₂ B : ℝ,
+      0 < C₂ ∧
+      (∀ (s : ℕ), (s ≥ B) →
+      let rs := Real.rpow s ((b : ℝ) / (b - 1))
+      ¬ (Ball (1 + Int.toNat <| Int.floor <| C₂ * rs)) (A 1) ⊆ Ball s (M ∪ (A 1)))
+    ) := by
+    sorry
 
 theorem theorem5
   (b : ℕ)
