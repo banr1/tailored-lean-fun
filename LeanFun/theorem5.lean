8f42e0fca6f6cc4c002be4293d576f0d699e581d
Vitaly Aksenov <aksenov@logicalintelligence.com>
2026-03-01 16:11:20 +0300

    Theorem 5 simplification

--- LeanFun/theorem5.lean
+++ LeanFun/theorem5.lean
@@ -12,6 +12,19 @@ def Sphere (n r : ℕ) : Set (FreeMonoid (Fin n)) :=
 noncomputable def expansion {n : ℕ} (G G' : Set (FreeMonoid (Fin n))) (s : ℕ) : ℕ :=
     sSup { r : ℕ | Ball r G ⊆ Ball s G' }
 
+theorem theorem5_simplification
+    (n : ℕ) (hn : 2 ≤ n) :
+    ∃ M : Set (FreeMonoid (Fin n)),
+      Tendsto
+        (fun r : ℕ =>
+          ((Set.ncard (M ∩ Sphere n r) : ℝ) / (Set.ncard (Sphere n r) : ℝ)))
+        atTop (nhds 0)
+      ∧
+      ∃ (K : ℕ), 0 < K ∧
+        (∀ᶠ r : ℕ in atTop,
+          Ball r (A n) ⊆ Ball (K * (Nat.log2 r) ^ 2) (M ∪ (A n))) := by
+  sorry
+
 theorem theorem5
     (n : ℕ) (hn : 2 ≤ n) :
     ∃ M : Set (FreeMonoid (Fin n)),
