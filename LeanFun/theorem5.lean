280054779d1c60acbf9c4a0f9b420990cde29167
Vitaly Aksenov <aksenov@logicalintelligence.com>
2026-03-01 20:40:12 +0300

    Had to comment theorem5 for Aleph not to cheat.

--- LeanFun/theorem5.lean
+++ LeanFun/theorem5.lean
@@ -25,23 +25,23 @@ theorem theorem5_simplification
           Ball r (A n) ⊆ Ball (K * (Nat.log2 r) ^ 2) (M ∪ (A n))) := by
   sorry
 
-theorem theorem5
-    (n : ℕ) (hn : 2 ≤ n) :
-    ∃ M : Set (FreeMonoid (Fin n)),
-      Tendsto
-        (fun r : ℕ =>
-          ((Set.ncard (M ∩ Sphere n r) : ℝ) / (Set.ncard (Sphere n r) : ℝ)))
-        atTop (nhds 0)
-      ∧
-      Tendsto
-        (fun s : ℕ =>
-          ((expansion (A n) (M ∪ (A n)) s : ℝ) / (s : ℝ)))
-        atTop atTop
-      ∧
-      ∃ (K : ℕ) (c : ℝ), 0 < K ∧ 0 < c ∧
-        (∀ᶠ r : ℕ in atTop,
-          Ball r (A n) ⊆ Ball (K * (Nat.log2 r) ^ 2) (M ∪ (A n)))
-        ∧
-        (∀ᶠ s : ℕ in atTop,
-          (Real.exp (c * Real.sqrt (s : ℝ)) ≤ (expansion (A n) (M ∪ (A n)) s : ℝ))) := by
-  sorry
+-- theorem theorem5
+--     (n : ℕ) (hn : 2 ≤ n) :
+--     ∃ M : Set (FreeMonoid (Fin n)),
+--       Tendsto
+--         (fun r : ℕ =>
+--           ((Set.ncard (M ∩ Sphere n r) : ℝ) / (Set.ncard (Sphere n r) : ℝ)))
+--         atTop (nhds 0)
+--       ∧
+--       Tendsto
+--         (fun s : ℕ =>
+--           ((expansion (A n) (M ∪ (A n)) s : ℝ) / (s : ℝ)))
+--         atTop atTop
+--       ∧
+--       ∃ (K : ℕ) (c : ℝ), 0 < K ∧ 0 < c ∧
+--         (∀ᶠ r : ℕ in atTop,
+--           Ball r (A n) ⊆ Ball (K * (Nat.log2 r) ^ 2) (M ∪ (A n)))
+--         ∧
+--        (∀ᶠ s : ℕ in atTop,
+--          (Real.exp (c * Real.sqrt (s : ℝ)) ≤ (expansion (A n) (M ∪ (A n)) s : ℝ))) := by
+--  sorry
