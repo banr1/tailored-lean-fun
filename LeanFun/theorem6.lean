ce3865da86528d2012a47c684c256889d14fbca6
Vitaly Aksenov <aksenov@logicalintelligence.com>
2026-03-16 22:09:54 +0300

    Theorem 6 statement has been fixed.

--- LeanFun/theorem6.lean
+++ LeanFun/theorem6.lean
@@ -5,41 +5,6 @@ open abelian
 def DoublyMacroSet (b : ℕ) : Set (FreeAbelianMonoid 1) :=
   { m | ∃ i : Fin 1, ∃ j : ℕ, m = Multiset.replicate (b ^ (b ^ j)) i }
 
-theorem theorem6_density (b : ℕ) (hb : 2 ≤ b) :
-  ∃ (d1 d2 : ℝ), ∀ (x : ℕ), x ≥ b ^ b →
-    0 < d1 ∧ 0 < d2 ∧
-      d1 * (Real.log (Real.log x)) ≤ ((DoublyMacroSet b) ∩ Ball x (A 1)).ncard ∧
-      ((DoublyMacroSet b) ∩ Ball x (A 1)).ncard ≤ d2 * (Real.log (Real.log x)) := by
-      sorry
-
-theorem theorem6_lower_bound (b : ℕ) (hb : 2 ≤ b) :
-  (∃ C₁ B : ℝ,
-    0 < C₁ ∧
-    (∀ (s : ℕ), (s ≥ B) →
-      let rs := Real.rpow s ((b : ℝ) / (b - 1))
-      (Ball (Int.toNat <| Int.ceil <| C₁ * rs) (A 1) ⊆ Ball s (M ∪ (A 1))))
-    ) := by
-    sorry
-
-theorem theorem6_upper_bound (b : ℕ) (hb : 2 ≤ b) :
-  (∃ C₂ B : ℝ,
-      0 < C₂ ∧
-      (∀ (s : ℕ), (s ≥ B) →
-      let rs := Real.rpow s ((b : ℝ) / (b - 1))
-      ¬ (Ball (1 + Int.toNat <| Int.floor <| C₂ * rs)) (A 1) ⊆ Ball s (M ∪ (A 1)))
-    ) := by
-    sorry
-
-theorem theorem6_upper_bound_neg (b : ℕ) (hb : 2 ≤ b) :
-  let M := DoublyMacroSet b
-  ¬ (∃ C₂ B : ℝ,
-      0 < C₂ ∧
-      (∀ (s : ℕ), (s ≥ B) →
-      let rs := Real.rpow s ((b : ℝ) / (b - 1))
-      ¬ (Ball (1 + Int.toNat <| Int.floor <| C₂ * rs)) (A 1) ⊆ Ball s (M ∪ (A 1)))
-    ) := by
-    sorry
-
 theorem theorem6
   (b : ℕ)
   (hb : 2 ≤ b) :
@@ -50,8 +15,8 @@ theorem theorem6
   (∃ C₁ C₂ B : ℝ,
     0 < C₁ ∧ 0 < C₂ ∧
     (∀ (s : ℕ), (s ≥ B) →
-      let rs := Real.rpow s ((b : ℝ) / (b - 1))
-      (Ball (Int.toNat <| Int.ceil <| C₁ * rs) (A 1) ⊆ Ball s (M ∪ (A 1))) ∧
-        ¬ (Ball (1 + Int.toNat <| Int.floor <| C₂ * rs)) (A 1) ⊆ Ball s (M ∪ (A 1)))
-    ) :=
+      let lb := Real.rpow s ((b : ℝ) / (b - 1))
+      (Ball (Int.toNat <| Int.ceil <| C₁ * lb) (A 1) ⊆ Ball s (M ∪ (A 1))) ∧
+        let ub := Real.rpow s ((2 * (b : ℝ) - 1) / (b - 1))
+        ¬ (Ball (1 + Int.toNat <| Int.floor <| C₂ * ub) (A 1) ⊆ Ball s (M ∪ (A 1))))) :=
   sorry
