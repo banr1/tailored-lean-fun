d7740cf2e30da78be46f2f85e1213b68deb06cf6
Vitaly Aksenov <aksenov@logicalintelligence.com>
2026-03-17 12:59:26 +0300

    Small change to theorem 6 statement.

--- LeanFun/theorem6.lean
+++ LeanFun/theorem6.lean
@@ -12,9 +12,9 @@ theorem theorem6
   (∃ (d1 d2 : ℝ), ∀ (x : ℕ), (x ≥ b ^ b) → 0 < d1 ∧ 0 < d2
       ∧ d1 * (Real.log (Real.log x)) ≤ (M ∩ (Ball x (A 1))).ncard
       ∧ (M ∩ (Ball x (A 1))).ncard ≤ d2 * (Real.log (Real.log x))) ∧
-  (∃ C₁ C₂ B : ℝ,
+  (∃ C₁ C₂ : ℝ,
     0 < C₁ ∧ 0 < C₂ ∧
-    (∀ (s : ℕ), (s ≥ B) →
+    (∀ (s : ℕ), (s ≥ 1) →
       let lb := Real.rpow s ((b : ℝ) / (b - 1))
       (Ball (Int.toNat <| Int.ceil <| C₁ * lb) (A 1) ⊆ Ball s (M ∪ (A 1))) ∧
         let ub := Real.rpow s ((2 * (b : ℝ) - 1) / (b - 1))
